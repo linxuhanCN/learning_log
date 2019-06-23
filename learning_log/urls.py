1b2c9346b6abd127d727d0cda8a040168f98301c
linxuhan <1119866938@qq.com>
2019-06-23 23:29:22 +0800

    user login

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -25,5 +25,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include(('users.urls','users'), namespace='users')),
     url(r'', include(('learning_logs.urls','learning_logs'), namespace='learning_logs')),
 ]
