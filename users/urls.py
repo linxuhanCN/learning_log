7f3272e9c77d75a289becd2710b0e454e77c0767
linxuhan <1119866938@qq.com>
2019-06-24 23:29:35 +0800

    register user

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     #登录页面
     #url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
     url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+    #注销
     url(r'^logout/$', views.logout_view, name='logout'),
+    #注册页面
+    url(r'^register/$', views.register, name='register'),
 ]
